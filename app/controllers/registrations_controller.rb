4b07921e7a7a903131a2a85077cdbc3eb2057a72
dcgarde <garde.danielle@gmail.com>
2022-05-08 22:22:00 -0500

    home, sign in, & sign up page running

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
     def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
     def account_user_update
         params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
     end
